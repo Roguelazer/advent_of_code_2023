f243ad23dbf6a8025769608f9495d177a5c94da0
James Brown <roguelazer@roguelazer.com>
2023-12-11 23:34:41 -0800

    day 12: ✔

--- src/bin/aoc12.rs
+++ src/bin/aoc12.rs
@@ -1,6 +1,8 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+const MAX_GROUPS: usize = 32;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 enum Spring {
     Broken,
     Working,
@@ -16,17 +18,9 @@ impl Spring {
             _ => panic!("unknown char {:?}", c),
         }
     }
-
-    fn unknown(&self) -> bool {
-        match *self {
-            Self::Broken => false,
-            Self::Working => false,
-            Self::Unknown => true,
-        }
-    }
 }
 
-fn s2i(ss: &[Spring]) -> u64 {
+fn s2i(ss: &[Spring]) -> u128 {
     let mut i = 0;
     for s in ss {
         if *s == Spring::Broken {
@@ -41,7 +35,30 @@ fn s2i(ss: &[Spring]) -> u64 {
 struct Row {
     springs: Vec<Spring>,
     groups: Vec<usize>,
-    total_broken: usize,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+struct CacheKey {
+    springs: u128,
+    springs_len: usize,
+    groups: [usize; MAX_GROUPS],
+    current_broken: usize,
+}
+
+impl CacheKey {
+    fn new(s: &[Spring], g: &[usize], current_broken: usize) -> Self {
+        assert!(g.len() < MAX_GROUPS);
+        let mut groups = [0usize; MAX_GROUPS];
+        for (i, gp) in g.iter().enumerate() {
+            groups[i] = *gp;
+        }
+        CacheKey {
+            springs: s2i(s),
+            springs_len: s.len(),
+            groups,
+            current_broken,
+        }
+    }
 }
 
 impl Row {
@@ -49,102 +66,66 @@ impl Row {
         if let Some((first, rest)) = s.split_once(' ') {
             let springs = first.chars().map(Spring::from_char).collect();
             let groups: Vec<usize> = rest.split(',').map(|w| w.parse().unwrap()).collect();
-            let total_broken = groups.iter().sum();
-            Row {
-                springs,
-                groups,
-                total_broken,
-            }
+            Row { springs, groups }
         } else {
             panic!("unhandled line {:?}", s);
         }
     }
 
-    fn valid(&self) -> bool {
-        if self
-            .springs
-            .iter()
-            .filter(|s| **s == Spring::Broken)
-            .count()
-            != self.total_broken
-        {
-            return false;
-        }
-        let mut git = self.groups.iter();
-        let mut current = 0;
-        let mut current_goal: usize = git.next().cloned().unwrap_or(0);
-        let mut saw_w = true;
-        for spring in &self.springs {
-            if *spring == Spring::Broken {
-                if current_goal == 0 {
-                    return false;
-                } else if current == 0 && !saw_w {
-                    // a new group can only start after a gap
-                    return false;
+    fn solutions_inner(
+        &self,
+        dp: &mut HashMap<CacheKey, usize>,
+        springs: &[Spring],
+        groups: &[usize],
+        current_broken: usize,
+    ) -> usize {
+        let key = CacheKey::new(springs, groups, current_broken);
+        if let Some(found) = dp.get(&key) {
+            return *found;
+        };
+
+        let val = match springs.split_first() {
+            Some((Spring::Working, rs)) => {
+                if current_broken == 0 {
+                    self.solutions_inner(dp, rs, groups, 0)
                 } else {
-                    current += 1;
-                }
-                saw_w = false;
-            } else {
-                saw_w = true;
-                if current != 0 {
-                    if current_goal != current {
-                        return false;
-                    } else {
-                        current = 0;
-                        current_goal = git.next().cloned().unwrap_or(0);
+                    match groups {
+                        [g, rg @ ..] if *g == current_broken => self.solutions_inner(dp, rs, rg, 0),
+                        _ => 0,
                     }
                 }
             }
-        }
-        let next = git.next();
-        current == current_goal && next.is_none()
+            Some((Spring::Broken, rs)) => {
+                return match groups {
+                    [g, ..] if current_broken > *g => 0,
+                    [] => 0,
+                    _ => self.solutions_inner(dp, rs, groups, current_broken + 1),
+                }
+            }
+            Some((Spring::Unknown, _)) => {
+                let mut new_working = springs.to_vec();
+                new_working[0] = Spring::Working;
+                let mut new_broken = springs.to_vec();
+                new_broken[0] = Spring::Broken;
+                let working_count = self.solutions_inner(dp, &new_working, groups, current_broken);
+                let broken_count = self.solutions_inner(dp, &new_broken, groups, current_broken);
+                working_count + broken_count
+            }
+            None => match groups {
+                [] if current_broken == 0 => 1,
+                [g] if *g == current_broken => 1,
+                _ => 0,
+            },
+        };
+
+        dp.insert(key, val);
+
+        val
     }
 
-    fn part1_solutions(&self) -> usize {
-        let unknown_indices = self
-            .springs
-            .iter()
-            .enumerate()
-            .filter_map(|(i, s)| if s.unknown() { Some(i) } else { None })
-            .collect::<Vec<_>>();
-        // println!("row: {:?}", self);
-
-        assert!(unknown_indices.len() > 0);
-        assert!(unknown_indices.len() < 32);
-
-        println!("got {:?} unknowns", unknown_indices.len());
-
-        let mut candidate = self.clone();
-
-        let mut options = HashSet::new();
-
-        for mut ival in 0..=(1 << unknown_indices.len()) {
-            for sindex in &unknown_indices {
-                let val = (ival & 0x1) == 0x1;
-                ival >>= 1;
-                candidate.springs[*sindex] = if val { Spring::Broken } else { Spring::Working }
-            }
-            if candidate.valid() {
-                // println!("valid solution: {:?}", candidate.springs);
-                options.insert(s2i(&candidate.springs));
-            }
-        }
-        // println!("found {:?} options", options.len());
-        options.len()
-    }
-
-    fn part2_solutions(&self) -> usize {
-        let unknown_indices = self
-            .springs
-            .iter()
-            .enumerate()
-            .filter_map(|(i, s)| if s.unknown() { Some(i) } else { None })
-            .collect::<Vec<_>>();
-
-        let mut git = self.groups.iter();
-
-        todo!();
+    fn num_solutions(&self) -> usize {
+        let mut cache = HashMap::new();
+        self.solutions_inner(&mut cache, &self.springs, &self.groups, 0)
     }
 
     fn to_part2(&self) -> Self {
@@ -156,12 +137,7 @@ impl Row {
             groups.extend_from_slice(&self.groups);
         }
         springs.pop();
-        let total_broken = groups.iter().sum();
-        Self {
-            springs,
-            groups,
-            total_broken,
-        }
+        Self { springs, groups }
     }
 }
 
@@ -169,10 +145,9 @@ fn main() -> anyhow::Result<()> {
     let stdin = std::io::stdin();
     let input = std::io::read_to_string(stdin)?;
     let rows = input.lines().map(Row::from_line).collect::<Vec<_>>();
-    let part1: usize = rows.iter().map(|r| r.part1_solutions()).sum();
+    let part1: usize = rows.iter().map(|r| r.num_solutions()).sum();
     println!("part 1: {}", part1);
-    let part2_rows = rows.iter().map(|r| r.to_part2()).collect::<Vec<_>>();
-    let part2: usize = part2_rows.iter().map(|r| r.part2_solutions()).sum();
+    let part2: usize = rows.iter().map(|r| r.to_part2().num_solutions()).sum();
     println!("part 2: {}", part2);
     Ok(())
 }
